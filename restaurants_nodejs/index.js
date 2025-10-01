ab5c768de6484fb5bc80fda5cca75a27e236e908
leviettien1808 <leviettien1808@gmail.com>
2025-10-01 16:44:05 +0700

    [Server] Routes and static files

--- restaurants_nodejs/index.js
+++ restaurants_nodejs/index.js
@@ -1,13 +1,23 @@
+const path = require("path");
+
 const bodyParser = require("body-parser");
 const express = require("express");
 const morgan = require("morgan");
 
 const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(morgan("combined"));
 app.use(bodyParser.urlencoded());
-app.use(adminRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(8000);
