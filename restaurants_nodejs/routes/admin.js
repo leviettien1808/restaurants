0cb4a6d3e6b4a6d8983a7d8a3e5a907452e38bce
leviettien1808 <leviettien1808@gmail.com>
2025-10-01 20:30:26 +0700

    [Server-Views] Additional products page

--- restaurants_nodejs/routes/admin.js
+++ restaurants_nodejs/routes/admin.js
@@ -4,8 +4,8 @@ const productsController = require("../controllers/products");
 
 const router = express.Router();
 
+router.get("/products", productsController.getProducts);
 router.get("/add-product", productsController.getAddProduct);
-
 router.post("/add-product", productsController.postAddProduct);
 
 module.exports = router;
