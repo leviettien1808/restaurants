4b83282db4b72e46d1cbefc189b8380932568106
leviettien1808 <leviettien1808@gmail.com>
2025-10-02 09:04:03 +0700

    [Server-Controllers] Handle edit product

--- restaurants_nodejs/controllers/products.js
+++ restaurants_nodejs/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Thêm sản phẩm",
     path: "/admin/add-product",
   });
@@ -23,11 +23,39 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
     .then(([rows, fieldData]) => {
-      res.render("products", {
+      res.render("admin/product-list", {
         products: rows,
         pageTitle: "Danh sách sản phẩm",
         path: "/admin/products",
       });
     })
     .catch((err) => console.log(err));
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId).then(([rows, fieldData]) => {
+    if (rows.length > 0) {
+      res.render("admin/edit-product", {
+        product: rows[0],
+        pageTitle: "Chỉnh sửa sản phẩm",
+        path: "/admin/products",
+      });
+    } else {
+      res.redirect("/admin/products");
+    }
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const { id, name, price, description } = req.body;
+  const product = new Product(id, name, description, price);
+  product
+    .save()
+    .then((result) => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log("🚀 ~ err:", err);
+    });
 };
--- restaurants_nodejs/routes/admin.js
+++ restaurants_nodejs/routes/admin.js
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get("/products", productsController.getProducts);
 router.get("/add-product", productsController.getAddProduct);
 router.post("/add-product", productsController.postAddProduct);
+router.get("/edit-product/:productId", productsController.getEditProduct);
+router.post("/edit-product", productsController.postEditProduct);
 
 module.exports = router;
