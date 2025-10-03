04d52fb97dcee7a6c3be2772f7d644b55989d0fd
leviettien1808 <leviettien1808@gmail.com>
2025-10-03 23:56:43 +0700

    [Server-Views] Update navigator and create order pages

--- restaurants_nodejs/public/js/main.js
+++ restaurants_nodejs/public/js/main.js
@@ -14,3 +14,21 @@ function menuToggleClickHandler() {
 
 backdrop.addEventListener("click", backdropClickHandler);
 menuToggle.addEventListener("click", menuToggleClickHandler);
+
+// ---------- Increase/Descrease the number of products ----------
+const $$ = document.querySelectorAll.bind(document);
+const decreaseQuantityProducts = $$(".decrease-quantity-product");
+const quantityProducts = $$(".quantity-product");
+const increaseQuantityProducts = $$(".increase-quantity-product");
+
+quantityProducts.forEach((quantityProduct, index) => {
+  increaseQuantityProducts[index].addEventListener("click", () => {
+    quantityProduct.value = parseInt(quantityProduct.value) + 1;
+  });
+
+  decreaseQuantityProducts[index].addEventListener("click", () => {
+    if (parseInt(quantityProduct.value) > 0) {
+      quantityProduct.value = parseInt(quantityProduct.value) - 1;
+    }
+  });
+});
