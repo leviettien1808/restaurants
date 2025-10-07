8cdf87df7904033705ab39dffbf2ad5273b8c852
leviettien1808 <leviettien1808@gmail.com>
2025-10-07 08:17:02 +0700

    [Server] Order flow

--- restaurants_nodejs/controllers/orders.js
+++ restaurants_nodejs/controllers/orders.js
@@ -1,11 +1,57 @@
+const moment = require("moment");
+
+const Order = require("../models/Orders");
+const Product = require("../models/products");
+const OrderItem = require("../models/order-item");
+
 exports.getOrders = (req, res, next) => {
-  const orders = [
-    {
-      tableNumber: 1,
-      total: 59000,
-      status: "Đang chờ",
-      time: "17:59",
-    },
-  ];
-  res.render("shop", { orders, pageTitle: "Shop", path: "/" });
+  Order.findAll()
+    .then((orders) => {
+      const handledOrders = orders
+        .filter(
+          (order) =>
+            moment(order.dataValues.createdAt).format("L") ===
+            moment().format("L")
+        )
+        .map((result) => result.dataValues)
+        .sort((a, b) => b.id - a.id);
+      res.render("shop/shop", {
+        orders: handledOrders,
+        pageTitle: "Shop",
+        path: "/",
+        moment,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.getAddOrder = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.render("shop/add-order", {
+        products,
+        pageTitle: "Tạo đơn mới",
+        path: "/add-order",
+      });
+    })
+    .catch((err) => console.log("🚀 ~ err:", err));
+};
+
+exports.postAddOrder = (req, res, next) => {
+  const { table_number, ...products } = req.body;
+  req.user
+    .createOrder({ table_number: +table_number })
+    .then((order) => {
+      const orderId = order.dataValues.id;
+      return Object.entries(products)
+        .filter(([_, value]) => +value > 0)
+        .map(([productId, quantity]) => ({ quantity, orderId, productId }));
+    })
+    .then((records) => {
+      return OrderItem.bulkCreate(records);
+    })
+    .then((results) => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log("🚀 ~ err:", err));
 };
--- restaurants_nodejs/index.js
+++ restaurants_nodejs/index.js
@@ -8,6 +8,10 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorsController = require("./controllers/errors");
 const sequelize = require("./util/database");
+const Product = require("./models/products");
+const User = require("./models/users");
+const Order = require("./models/Orders");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -17,13 +21,38 @@ app.use(morgan("combined"));
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorsController.get404Page);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+Product.belongsToMany(Order, { through: OrderItem });
+
 sequelize
   .sync()
   .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Tien", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(8000);
   })
   .catch((err) => {
